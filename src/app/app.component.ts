5239b3564b9a9e149a6d5a74240b465536ae0568
godxvincent <godxvincent@gmail.com>
2020-11-25 16:14:57 -0500

    :sparkles: changes to handle french an spanish local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Ricardo Andrés Vargas';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 12345.5;
+  heroe = {
+    nombre : 'logan',
+    clave : 'Wolverine',
+    edad : 500,
+    direccion : {
+      calle : 'primera',
+      casa : 20
+    }
+  };
+  valorPromesa = new Promise<string> ((resolve, reject) => {
+    setTimeout( () => {
+      resolve('Llego la data');
+    }, 4500);
+  });
+
+  fecha = new Date();
+  idioma = 'es';
+
+  setSpanish(): void {
+    this.idioma = 'es';
+  }
+  setEnglish(): void {
+    this.idioma = 'en';
+  }
+  setFrench(): void {
+    this.idioma = 'fr';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import '@angular/common/locales/global/es';
+import '@angular/common/locales/global/fr';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
